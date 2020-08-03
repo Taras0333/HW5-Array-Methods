97f4f99a1fad961b2de335bf355ad8f523a16106
Taras <khomynt97@gmail.com>
2020-08-03 23:56:17 +0300

    2

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-let numberArray = [];
+
 //1
 
 function getRandomArray(length, min, max){
+	let numberArray = [];
 	for (let i = 0; i <= length -1; i++){
 	let random = 0;
 	random =  Math.floor(Math.random() * ((+max) - (+min) + 1)) + (+min);
 	numberArray.push(random);
-	console.log('random number:', random);
 }
 	return numberArray;
 }
 console.log(getRandomArray(prompt('Write the length'), prompt('Write min integer'), prompt('Write max integer')));
 
-function getModa(numbers) {
-	let firstNumber = numbers.filter(() => numbers > 2);
-	console.log(firstNumber);
-}
-getModa(numberArray);
-
 //3
 function getAverage(quontity, numbers){
-	console.log(quontity);
 	let sum = 0;
 	let array = [];
 
 	for (let i = 0; i <= quontity - 2; i++){
  	if (Number.isInteger(numbers)){
- 		console.log('numbers:', numbers)
  		sum += numbers;
  		array.push(numbers);
- 			numbers = +prompt('Write number');
+ 		numbers = +prompt('Write number');
  	} else{
  		sum += 0
  		alert('its not an integer');
@@ -37,15 +29,12 @@ function getAverage(quontity, numbers){
  	}
  }
  if (Number.isInteger(numbers)){
- 		console.log('numbers:', numbers)
  		sum += numbers;
  		array.push(numbers);
  	} else{
  		sum += 0
  		alert('its not an integer');
  	}
-
- console.log(array);
  return sum / quontity;
 
 
@@ -57,7 +46,6 @@ console.log(getAverage(+prompt('how long?'), +prompt('Write number')));
 function getMediana (quontity, number){
 let array = [];
 let medianaIndex = 0;
-let averageNumber = 0;
 let result = 0;
 for (let i =0; i <= quontity - 2; i++){
 	if (Number.isInteger(number)){
@@ -86,8 +74,6 @@ for (let i =0; i <= quontity - 2; i++){
 		medianaIndex = array.length / 2 + 0.5;
 		result = array[medianaIndex - 1];
 	}
-	console.log(array);
-	console.log(medianaIndex);
 	return result;
 }
 console.log(getMediana(+prompt('how long?'), +prompt('Write number')));
@@ -101,7 +87,6 @@ function filterEvenNumbers(quontity ,numbers){
 		numbers = +prompt('Write numbers');
 	}
 	array.push(numbers);
-	console.log(array); 
 	const filter = array.filter((el, i, arr) => {
 		if (el % 2) return true;
 			else return false;
@@ -116,13 +101,11 @@ function countPositiveNumbers(quontity ,numbers){
 		array.push(numbers);
 		numbers = +prompt('Write numbers');
 	}
-	array.push(numbers);
-	console.log(array); 
+	array.push(numbers); 
 	const numbersBiggerThanNull = array.filter((el, i, arr) => {
 		if (el > 0) return true;
 			else return false;
 	})
-	console.log(numbersBiggerThanNull);
 	const result = numbersBiggerThanNull.length;
 	return result;
 
@@ -131,13 +114,11 @@ console.log(countPositiveNumbers(+prompt('How much'), +prompt('Enter numbers')))
 //7
 function getDivededByFive(quontity ,numbers){
 	let array = [];
-	let arrayDivvededByFive =[];
 	for (let i = 0; i <= quontity - 2; i++ ){
 		array.push(numbers);
 		numbers = +prompt('Write numbers');
 	}
 	array.push(numbers);
-	console.log(array); 
 	const divedByFive = array.filter((el, i, arr) => {
 		if (Number.isInteger(el / 5)) return true;
 			else return false;
